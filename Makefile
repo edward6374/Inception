26f1d67bd02c2849de21faddda01dfb102f84bca
Valerio Duchi <vduchi@student.42barcelona.com>
2024-02-23 18:10:06 +0100

    Project completed, now for testing

--- Makefile
+++ Makefile
@@ -1,3 +1,14 @@
+DIR = srcs
+DCK_EXEC = /usr/bin/docker
 
-all:
-	
+up:
+	@cd $(DIR) && $(DCK_EXEC) compose up
+
+upd:
+	@cd $(DIR) && $(DCK_EXEC) compose up -d
+
+down:
+	@cd $(DIR) && $(DCK_EXEC) compose down
+
+clean:
+	$(DCK_EXEC) images --format "{{.Repository}}" | awk '{print $1}' | xargs -I {} docker rmi {}
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,38 +1,15 @@
 #!bin/bash
 
-echo "WP User: -$WORDPRESS_DB_NAME-"
-echo "WP Database: -$WORDPRESS_DB_HOST-"
-if [ ! -f "/var/www/html/config.php" ]; then
-	cd /var/www/html
+if [ -f ./wp-config.php ]
+then
+	echo "Wordpress already exists"
+else
 	wp core download --allow-root
-	echo ""
-	echo "WP DOWNLOADED"
-	echo ""
-	echo "Host: -$WORDPRESS_DB_HOST-"
-	echo "Name: -$WORDPRESS_DB_NAME-"
-	echo "User: -$WORDPRESS_DB_USER-"
-	echo "Password: -$WORDPRESS_DB_PWD-"
-	echo "Domain name: -$DOMAIN_NAME-"
-	echo "Title: -$TITLE-"
-	echo "Admin user: -$ADMIN_WP_USER-"
-	echo "Admin password: -$ADMIN_WP_PWD-"
-	echo "Admin email: -$ADMIN_WP_EMAIL-"
-	echo "Normal user: -$WP_NORMAL_USER-"
-	echo "Normal mail: -$WP_NORMAL_EMAIL-"
-	echo "Normal password: -$WP_NORMAL_PWD-"
-	wp config create --dbhost=$WORDPRESS_DB_HOST --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PWD --dbcharset="utf8" --allow-root
-	echo ""
-	echo "WP CONFIG CREATED"
-	echo ""
-	#wp core install --url=$DOMAIN_NAME --title=$TITLE --admin_user=$ADMIN_WP_USER --admin_password=$ADMIN_WP_PWD --admin_email=$ADMIN_WP_EMAIL --allow-root
-	#echo ""
-	#echo "WP CORE INSTALLED"
-	#echo ""
-	#wp user create $WP_NORMAL_USER $WP_NORMAL_EMAIL --role=author --user_pass=$WP_NORMAL_PWD --allow-root
-	#echo ""
-	#echo "WP USER CREATED"
-	#echo ""
+	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOSTNAME --allow-root
+	wp core install --url=$DONAIN_NAME --title="$WORDPRESS_TITLE" --admin_user=$WORDPRESS_ADMIM --admin_password=$WORDPRESS_ADMIM_PASS  --admin_email=$WORDPRESS_ADMIM_EMAIL --skip-email --allow-root
+	wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASS --allow-root
+	wp theme install twentysixteen --activate --allow-root
 fi
 
 echo "Wordpress is set up! Now running"
-#/usr/sbin/php-fpm7.4 -F
+/usr/sbin/php-fpm7.4 -F
